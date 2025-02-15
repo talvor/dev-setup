4f443a22597db6fe3a3d28181f0394f4930c04b3
Phillip Hall <talvor@me.com>
2025-02-15 20:02:51 +1100

    Add copilot to nvim

--- dotfiles/neovim/.config/nvim/lua/community.lua
+++ dotfiles/neovim/.config/nvim/lua/community.lua
@@ -8,6 +8,7 @@ return {
   -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   { import = "astrocommunity.motion.harpoon" },
 
