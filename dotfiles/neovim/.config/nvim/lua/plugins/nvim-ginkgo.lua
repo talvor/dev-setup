9f9b97b38bae5ca1067d4020e149b7e976121359
Phillip Hall <talvor@me.com>
2025-04-15 21:29:47 +1000

    Replace AstroNvim with LazyVim

--- dotfiles/neovim/.config/nvim/lua/plugins/nvim-ginkgo.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-neotest/neotest",
-  optional = true,
-  dependencies = { "nvim-contrib/nvim-ginkgo", config = function() end },
-  opts = function(_, opts)
-    if not opts.adapters then opts.adapters = {} end
-    table.insert(opts.adapters, require "nvim-ginkgo")
-  end,
-}
