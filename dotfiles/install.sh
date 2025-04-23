471baf88d2b14850d71b66e26821e1ccb6d7848b
Phillip Hall <talvor@me.com>
2025-04-23 20:38:01 +1000

    Add sway config

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -5,12 +5,16 @@ stow --target=$HOME bash
 stow --target=$HOME ghostty
 stow --target=$HOME git
 stow --target=$HOME neovim
+stow --target=$HOME rofi
 stow --target=$HOME starship
+stow --target=$HOME sway
 stow --target=$HOME tmux
+stow --target=$HOME waybar
+stow --target=$HOME wlogout
 stow --target=$HOME zsh
 
 if [ -f $HOME/.bashrc ]; then
-	sed -zi '/source\s~\/\.bashrc\.d\/loadrc/!s/$/\nsource ~\/\.bashrc\.d\/loadrc\n/' ~/.bashrc
+  sed -zi '/source\s~\/\.bashrc\.d\/loadrc/!s/$/\nsource ~\/\.bashrc\.d\/loadrc\n/' ~/.bashrc
 fi
 
 # if [ -f $HOME/.zshrc ]; then
@@ -19,27 +23,27 @@ fi
 
 mkdir -p $HOME/.local/share/zsh
 if [ ! -f $HOME/.local/share/zsh/antigen.zsh ]; then
-	echo "Installing antigen"
-	curl -L git.io/antigen >$HOME/.local/share/zsh/antigen.zsh
+  echo "Installing antigen"
+  curl -L git.io/antigen >$HOME/.local/share/zsh/antigen.zsh
 fi
 
 mkdir -p $HOME/.tmux
 if [ ! -d $HOME/.tmux/plugins ]; then
-	# Install tmux plugin manager
-	echo "Installing tpm"
-	mkdir -p $HOME/.tmux/plugins
-	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  # Install tmux plugin manager
+  echo "Installing tpm"
+  mkdir -p $HOME/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 if [ ! -d $HOME/.nvm ]; then
-	# Install node version manager
-	echo "Installing nvm"
-	curl -sS https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | sh
+  # Install node version manager
+  echo "Installing nvm"
+  curl -sS https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | sh
 fi
 
 if [ ! -d $HOME/.fzf-git ]; then
-	# Install fzf-git
-	echo "Installing fzf-git"
-	mkdir -p $HOME/.fzf-git
-	git clone https://github.com/junegunn/fzf-git.sh $HOME/.fzf-git
+  # Install fzf-git
+  echo "Installing fzf-git"
+  mkdir -p $HOME/.fzf-git
+  git clone https://github.com/junegunn/fzf-git.sh $HOME/.fzf-git
 fi
--- dotfiles/uninstall.sh
+++ dotfiles/uninstall.sh
@@ -4,5 +4,6 @@ stow --target=$HOME --delete alacritty
 stow --target=$HOME --delete bash
 stow --target=$HOME --delete ghostty
 stow --target=$HOME --delete neovim
+stow --target=$HOME --delete rofi
 stow --target=$HOME --delete tmux
 stow --target=$HOME --delete zsh
